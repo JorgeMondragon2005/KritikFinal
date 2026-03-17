74cfa2c2e3ea8a08830fc8fc597c8fef0c18b8ca
Jorge Mondragon <jorgemondra242@gmail.com>
2026-03-17 16:56:04 -0600

    fix(api): fix camelCase json payload deserialization for C# models

--- src/Kritik.Backend/Program.cs
+++ src/Kritik.Backend/Program.cs
@@ -45,7 +45,10 @@ builder.Services.AddCors(options =>
         });
 });
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
